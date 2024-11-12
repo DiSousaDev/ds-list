44c4f5166265cc59b05dcc4affce0168cd12e304
Diego Sousa <me@diego.dev.br>
2024-11-11 23:01:43 -0300

    Initial project

---
+++ src/test/java/br/dev/diego/dslist/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.diego.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
